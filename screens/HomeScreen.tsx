446f7dddfd44bfff74924f93f4ed0eaa1ca9e17a
Filip Jerga <jerga99@gmail.com>
2022-01-10 11:59:35 +0000

    Native stack header props

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,9 @@
 
 import { useEffect } from "react";
 import { View, Text, Button } from "react-native";
+import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 
-export default function HomeScreen({navigation}: any) {
+export default function HomeScreen({navigation}: NativeStackHeaderProps) {
 
   useEffect(() => {
     console.log("Rending Home Screen");
--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,8 +1,9 @@
 
 import { useEffect } from "react";
 import { View, Text, Button } from "react-native";
+import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 
-export default function PlannerScreen({navigation}: any) {
+export default function PlannerScreen({navigation}: NativeStackHeaderProps) {
 
   useEffect(() => {
     console.log("Rending Planner Screen");

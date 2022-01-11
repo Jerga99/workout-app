183244d619a9332bb72f071dc2a91810721e3112
Filip Jerga <jerga99@gmail.com>
2022-01-11 12:19:42 +0000

    workout item styles

--- components/WorkoutItem.tsx
+++ components/WorkoutItem.tsx
@@ -1,13 +1,41 @@
 
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { Workout } from "../types/data";
 
 export default function WorkoutItem({item}: {item: Workout}) {
   return (
-    <View>
-      <Text>{item.name}</Text>
-      <Text>{item.difficulty}</Text>
+    <View style={styles.container}>
+      <Text
+        style={styles.name}>{item.name}
+      </Text>
+      <Text
+        style={styles.duration}>Duration: {item.duration}
+      </Text>
+      <Text
+        style={styles.difficulty}>Difficulty: {item.difficulty}
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 10,
+    borderColor: "rgba(0,0,0, 0.1)",
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  duration: {
+    fontSize: 15,
+  },
+  difficulty: {
+    fontSize: 15
+  }
+})
 

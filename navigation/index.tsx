5ea774c4e3ce355e535a4ebc010d6ac7d002def5
Filip Jerga <jerga99@gmail.com>
2022-01-11 10:03:40 +0000

    Bottom Tab Navigator

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -2,27 +2,45 @@
 
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 
 import HomeScreen from "../screens/HomeScreen";
 import PlannerScreen from "../screens/PlannerScreen";
 
-const Stack = createNativeStackNavigator();
-
 export default function Navigation() {
-
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
-        <Stack.Screen
-          name="Planner"
-          component={PlannerScreen}
-        />
-      </Stack.Navigator>
+      <RootNavigator />
     </NavigationContainer>
   )
 }
+
+const Stack = createNativeStackNavigator();
+function RootNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Root"
+        component={BottomTabNavigator}
+      />
+    </Stack.Navigator>
+  )
+}
+
+const BottomTab = createBottomTabNavigator();
+function BottomTabNavigator() {
+  return (
+    <BottomTab.Navigator initialRouteName="Home">
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+      />
+      <BottomTab.Screen
+        name="Planner"
+        component={PlannerScreen}
+      />
+    </BottomTab.Navigator>
+  )
+}
+

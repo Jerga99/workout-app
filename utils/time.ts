aa82fdd1177594bc53255108d96abffbaa9f39a4
Filip Jerga <jerga99@gmail.com>
2022-01-12 11:56:06 +0000

    Improve format

--- utils/time.ts
+++ utils/time.ts
@@ -9,5 +9,23 @@ export function formatSec(sec: number) {
   const _min = Math.floor(sec / 60);
   const _sec = sec % 60;
 
-  return `${_min} min and ${_sec} sec`
+  const minText = `${_min} min`;
+  const secText = `${_sec} sec`;
+  const connector = "and"
+
+  const sentence = [];
+
+  if (_min > 0) {
+    sentence.push(minText);
+  }
+
+  if (_min > 0 && _sec > 0) {
+    sentence.push(connector);
+  }
+
+  if (_sec > 0) {
+    sentence.push(secText);
+  }
+
+  return sentence.join(" ");
 }

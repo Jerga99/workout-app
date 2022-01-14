33914b7abc99e1daded30c5f7b25cb120f848ddf
Filip Jerga <jerga99@gmail.com>
2022-01-14 09:53:30 +0000

    UseIsFocused

--- hooks/useWorkouts.ts
+++ hooks/useWorkouts.ts
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
 import { getWorkouts } from "../storage/workout";
 import { Workout } from "../types/data";
+import { useIsFocused } from "@react-navigation/native";
 
 export const useWorkouts = () => {
   const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const isFocused = useIsFocused();
 
   useEffect(() => {
     async function getData() {
+      console.log("Getting Data!");
       const _workouts = await getWorkouts();
       setWorkouts(_workouts);
     }
 
-    getData();
-  }, [])
+    if (isFocused) {
+      getData();
+    }
+  }, [isFocused])
 
   return workouts;
 }

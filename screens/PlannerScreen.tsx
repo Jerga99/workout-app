3533d04cbffa94e50ba00417b16b6567df024955
Filip Jerga <jerga99@gmail.com>
2022-01-20 10:37:57 +0000

    slugify

--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -3,12 +3,13 @@ import { View, StyleSheet } from "react-native";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import ExerciseForm, { ExerciseFormData } from "../components/ExerciseForm";
 import { SequenceItem, SequenceType } from "../types/data";
+import slugify from "slugify";
 
 export default function PlannerScreen({navigation}: NativeStackHeaderProps) {
 
   const handleFormSubmit = (form: ExerciseFormData) => {
     const sequenceItem: SequenceItem = {
-      slug: form.name + Date.now(),
+      slug: slugify(form.name + " " + Date.now(), {lower: true}),
       name: form.name,
       type: form.type as SequenceType,
       duration: Number(form.duration)

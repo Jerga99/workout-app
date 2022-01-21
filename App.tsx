8680b29a5864faef190935bf6b0acdd751092c2f
Filip Jerga <jerga99@gmail.com>
2022-01-21 09:09:47 +0000

    Theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from 'react-native';
 import useCachedResources from './hooks/useCachedResources';
 import Navigation from "./navigation"
 
 export default function App() {
   const isLoaded = useCachedResources();
+  const theme = useColorScheme();
+
+  console.log(theme);
 
   if (isLoaded) {
     return (
